e94ae35776ba6a55bd5b3201b352c2b864df8a81
k-przyk <keoni.spencer@yale.edu>
2024-05-01 17:43:29 -0400

    changes

---
+++ controller/CMakeFiles/auto_driver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/auto_driver.dir/src/auto_driver.cpp.o"
+  "CMakeFiles/auto_driver.dir/src/auto_driver.cpp.o.d"
+  "bin/auto_driver"
+  "bin/auto_driver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/auto_driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ controller/CMakeFiles/cmd_reciever.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmd_reciever.dir/src/cmd_reciever.cpp.o"
+  "CMakeFiles/cmd_reciever.dir/src/cmd_reciever.cpp.o.d"
+  "bin/cmd_reciever"
+  "bin/cmd_reciever.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmd_reciever.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
