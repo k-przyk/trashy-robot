1647d7e4f77d1a46b988a29d84a32f3e6b8079db
k-przyk <keoni.spencer@yale.edu>
2024-05-06 22:58:01 -0400

    Smaller Window

--- camera/src/camera.cpp
+++ camera/src/camera.cpp
@@ -53,8 +53,8 @@ int main() {
     stereo->setSubpixel(false);
 
     // Roi config
-    dai::Point2f topLeft(0.45f, 0.6f);
-    dai::Point2f bottomRight(0.55f, 1.0f);
+    dai::Point2f topLeft(0.45f, 0.7f);
+    dai::Point2f bottomRight(0.55f, 0.9f);
 
     dai::SpatialLocationCalculatorConfigData config;
     config.depthThresholds.lowerThreshold = 100;
