f1dc22b4e39f6d9831b1c4f6919f2397973b1358
k-przyk <keoni.spencer@yale.edu>
2024-05-06 22:53:40 -0400

    Lowering drive frame

--- camera/src/camera.cpp
+++ camera/src/camera.cpp
@@ -53,8 +53,8 @@ int main() {
     stereo->setSubpixel(false);
 
     // Roi config
-    dai::Point2f topLeft(0.4f, 0.7f);
-    dai::Point2f bottomRight(0.6f, 0.95f);
+    dai::Point2f topLeft(0.45f, 0.6f);
+    dai::Point2f bottomRight(0.55f, 1.0f);
 
     dai::SpatialLocationCalculatorConfigData config;
     config.depthThresholds.lowerThreshold = 100;
