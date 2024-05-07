0ee58b84b4fd35c4233e3aabf352ea18f6ef01d7
k-przyk <keoni.spencer@yale.edu>
2024-05-07 16:04:54 -0400

    Depth syncing

--- camera/src/camera.cpp
+++ camera/src/camera.cpp
@@ -92,7 +92,7 @@ void pub_depth(zmq::context_t* context,
         if (depthZ > lastDepth * 2) {
             // The region hasn't aligned yet
             depthZ = lastDepth; // Dangerous code but better than sudden acceleration
-        } else if (depth != 0) {
+        } else if (depthZ != 0) {
             lastDepth = depthZ;
         }
 
